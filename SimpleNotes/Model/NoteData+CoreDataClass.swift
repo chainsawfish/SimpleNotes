64104f62696f2b31fe843972b7b36da6431f502e
Denis <chainsawfish@gmail.com>
2022-05-18 11:54:32 +0700

    Added core data and tableView

---
+++ SimpleNotes/Model/NoteData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  NoteData+CoreDataClass.swift
+//  SimpleNotes
+//
+//  Created by Denis Borneman on 18.05.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(NoteData)
+public class NoteData: NSManagedObject {
+
+}
